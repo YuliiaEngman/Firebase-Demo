48a672cc1e21296be1d8d9c6c73c33bba0c09733
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-02 10:50:37 -0500

    adde UIViewController+Navigation file

--- Firebase-Demo/Controllers/UIViewController+Navigation.swift
+++ Firebase-Demo/Controllers/UIViewController+Navigation.swift
@@ -10,4 +10,19 @@ import UIKit
 
 extension UIViewController {
     
+    private static func resetWindow(with rootviewcontroller: UIViewController) {
+        guard let scene = UIApplication.shared.connectedScenes.first,
+        let sceneDelegate = scene.delegate as? SceneDelegate,
+            let window = sceneDelegate.window else {
+                fatalError("could not reset window rootViewController")
+        }
+        window.rootViewController = rootviewcontroller
+    }
+    
+    public static func showViewController(storyboardName: String, viewControllerId: String) {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let newVC = storyboard.instantiateViewController(withIdentifier: viewControllerId)
+        resetWindow(with: newVC)
+    }
+    
 }
