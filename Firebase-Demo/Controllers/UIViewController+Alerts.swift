ac1afbf3c926396a260ccf00b1e3418a39f4b2ab
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-02 10:38:51 -0500

    added view controllers files

---
+++ Firebase-Demo/Controllers/UIViewController+Alerts.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Alerts.swift
+//  Firebase-Demo
+//
+//  Created by Yuliia Engman on 3/2/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
---
+++ Firebase-Demo/Controllers/UIViewController+Navigation.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Navigation.swift
+//  Firebase-Demo
+//
+//  Created by Yuliia Engman on 3/2/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
