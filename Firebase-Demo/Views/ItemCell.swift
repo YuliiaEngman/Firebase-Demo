dd972341e774c1dfac748c448b7526aa1de8f328
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-04 10:31:43 -0500

    created itemCell xib file and UITableViewCell

---
+++ Firebase-Demo/Views/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  Firebase-Demo
+//
+//  Created by Yuliia Engman on 3/4/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: DesignableImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var sellerNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    public func configureCell(for item: Item) {
+        itemNameLabel.text = item.itemName
+        sellerNameLabel.text = "@\(item.sellerName)"
+        dateLabel.text = item.listedDate.description
+        let price = String(format: "%.2f", item.price)
+        priceLabel.text = "$\(price)"
+    }
+}
