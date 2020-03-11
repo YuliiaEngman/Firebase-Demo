63c6a91528645998f3edd86e670b8fd39fefeffa
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-11 14:59:08 -0400

    added new VC

---
+++ Firebase-Demo/Controllers/ItemDetailController.swift
@@ -0,0 +1,19 @@
+//
+//  ItemDetailController.swift
+//  Firebase-Demo
+//
+//  Created by Yuliia Engman on 3/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
